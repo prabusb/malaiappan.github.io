82b1aa8c58d1e9d5114d802fee23222c5341042a
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-23 12:21:30 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -33,36 +33,49 @@ saveSearches = function() {
     var searchedRoutes = JSON.stringify(searches);
     localStorage.searchedRoutes = searchedRoutes;
 };
-  
-getData = function() {
+
+getDataOnSubmit = function () {
 	var from = document.getElementById("from").value;
 	var to = document.getElementById("to").value;
 	var date = document.getElementById("date").value;
+	var url = 'https://flightmock.scalingo.io/flights/' + from + '/' + to + '/' + date;
 	
-    var url = 'https://flightmock.scalingo.io/flights/' + from + '/' + to + '/' + date;
-	
-	searches.push({"from":from, "to":to, "url":url})
+	searches.push({"from":from, "to":to, "date":date, "url":url})
 	saveSearches();
 	
-    // TODO add cache logic here
+	getData(url);
+}
+
+getDataFromLocalStorage = function () {
+	var searchedRoutes = JSON.parse(localStorage.searchedRoutes)
+	var length = searchedRoutes.length;
+		
+	var url = searchedRoutes[length-1].url;
+	
+	getData(url);
+}
+  
+getData = function(url) {
+	
+   // var url = 'https://flightmock.scalingo.io/flights/' + from + '/' + to + '/' + date;
+	
     if ('caches' in window) {
       /*
-       * Check if the service worker has already cached this city's weather
-       * data. If the service worker has the data, then display the cached
+       * Check if the service worker has already cached this  data. If the service worker has the data, then display the cached
        * data while the app fetches the latest data.
        */
       caches.match(url).then(function(response) {
         if (response) {
           response.json().then(function updateFromCache(json) {
-			  console.log("from cache" + json)
+		  console.log("from cache" + json)
 		  console.log("cache size" + json.length)
-			 var html = "<table  class=\"striped\" ><thead><tr><th data-field=\"id\">From</th><th data-field=\"name\">To</th><th data-field=\"price\">Date</th><th data-field=\"price\">Time</th><th data-field=\"price\">Price</th><th></th></tr></thead><tbody>";
-			 for(var i = 0, l = json.length; i < l; i++) {
-				html = html + "<tr><td>" + json[i].departureSation.stationCode + "</td><td>" + json[i].arrivalStation.stationCode + "</td><td>" + json[i].departureDate + "</td><td>" + json[i].departureTime + "</td><td>" + json[i].price.currency + " " + json[i].price.price + "</td><td> <a  id=\"btnmod\" class=\"waves-effect waves-light btn\">Book</a></td>";
-			 }
-			 document.getElementById("flightinfo").innerHTML = html + "</tbody></table>"
-			 console.log("filled from cache")
-			  return;
+		 var html = "<table  class=\"striped\" ><thead><tr><th data-field=\"id\">From</th><th data-field=\"name\">To</th><th data-field=\"price\">Date</th><th data-field=\"price\">Time</th><th data-field=\"price\">Price</th><th></th></tr></thead><tbody>";
+		 for(var i = 0, l = json.length; i < l; i++) {
+			html = html + "<tr><td>" + json[i].departureSation.stationCode + "</td><td>" + json[i].arrivalStation.stationCode + "</td><td>" + json[i].departureDate + "</td><td>" + json[i].departureTime + "</td><td>" + json[i].price.currency + " " + json[i].price.price + "</td><td> <a  id=\"btnmod\" class=\"waves-effect waves-light btn\">Book</a></td>";
+		 }
+		 document.getElementById("flightinfo").innerHTML = html + "</tbody></table>"
+		 console.log("filled from cache")
+		  return;
           });
         }
       });
@@ -73,20 +86,18 @@ getData = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
           var response = JSON.parse(request.response);
-		  console.log("from request" + response)
-		  console.log("size from network" + response.length)
+	  console.log("from request" + response)
+	  console.log("size from network" + response.length)
 		  
-		var html = "<table  class=\"striped\" ><thead><tr><th data-field=\"id\">From</th><th data-field=\"name\">To</th><th data-field=\"price\">Date</th><th data-field=\"price\">Time</th><th data-field=\"price\">Price</th><th></th></tr></thead><tbody>";
-			 for(var i = 0, l = response.length; i < l; i++) {
-				html = html + "<tr><td>" + response[i].departureSation.stationCode + "</td><td>" + response[i].arrivalStation.stationCode + "</td><td>" + response[i].departureDate + "</td><td>" + response[i].departureTime + "</td><td>" + response[i].price.currency + " " + response[i].price.price + "</td><td> <a  id=\"btnmod\" class=\"waves-effect waves-light btn\">Book</a></td>";
-			 }
-			 document.getElementById("flightinfo").innerHTML = html + "</tbody></table>"
-			 console.log("filled from network")
-        }
+	  var html = "<table  class=\"striped\" ><thead><tr><th data-field=\"id\">From</th><th data-field=\"name\">To</th><th data-field=\"price\">Date</th><th data-field=\"price\">Time</th><th data-field=\"price\">Price</th><th></th></tr></thead><tbody>";
+	   for(var i = 0, l = response.length; i < l; i++) {
+		html = html + "<tr><td>" + response[i].departureSation.stationCode + "</td><td>" + response[i].arrivalStation.stationCode + "</td><td>" + response[i].departureDate + "</td><td>" + response[i].departureTime + "</td><td>" + response[i].price.currency + " " + response[i].price.price + "</td><td> <a  id=\"btnmod\" class=\"waves-effect waves-light btn\">Book</a></td>";
+	   }
+	   document.getElementById("flightinfo").innerHTML = html + "</tbody></table>"
+	   console.log("filled from network")
+       }
       } else {
-		  console.log("error for " + url)
-        // Return the initial weather forecast since no data is available.
-       // app.updateForecastCard(initialWeatherForecast);
+	 console.log("error for " + url)
       }
     };
     request.open('GET', url);
