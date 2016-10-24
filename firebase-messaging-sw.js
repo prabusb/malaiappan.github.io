e9055ee126967d0375225136f4a6d4ada50b055b
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-24 11:49:04 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,22 @@
 
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
+
+firebase.initializeApp({
+  'messagingSenderId': '1031604326543'
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Your BoardinPass for';
+  const notificationOptions = {
+    body: 'Checkin completed & boarding pass is ready',
+    icon: '/images/Ebp.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
