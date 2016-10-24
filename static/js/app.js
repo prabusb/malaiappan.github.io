eb8addd4caa5dd0f8dde65f82307069386ba3d85
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-24 14:42:01 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -14,6 +14,7 @@ document.getElementById("viewoffers").addEventListener('click', function() {
 document.getElementById("viewRecentSearch").addEventListener('click', function() {
     $('.button-collapse').sideNav('hide');
 	$("main").find("div.container1:visible").hide();
+	 $('#backtosearchdiv').hide('fadeOut');
     showMySearches();
 	$("#recentSearches").show('fadeIn');
   });
