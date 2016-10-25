7bdc1cbdde6b508a113cf4653c685c13231f0463
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-25 17:45:10 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,5 @@
-
-<script type="text/javascript" src="https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js"></script>
-<script type="text/javascript" src="https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js"></script>
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
 
 firebase.initializeApp({
   'messagingSenderId': '1031604326543'
