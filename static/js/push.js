759a3e76ab753020d0fc04eb24d71dda2dc839ca
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-25 17:47:55 +0200

    Update push.js

--- static/js/push.js
+++ static/js/push.js
@@ -1,162 +1,140 @@
-(function (window) {
-  'use strict';
+/**
+ * jQuery ImageCaching plugin
+ * Caching selected images in localStorage
+ *
+ * Created by Yosef(Vlad) Kaminskyi
+ * Mailto: moledet[at]ukr.net
+ * Version:  1.0  on 03/05/2015.
+ * Dependencies:
+ *      jQuery https://jquery.com/
+ */
+;(function($) {
+    function jQueryImageCaching(params){
+        var ImageCaching = {
+            selector: 'img',
+            debugMode: false,
+            cachingKeyAttribute:'data-caching-key',
+            sourceKeyAttribute:'data-src',
+            renderCallback:null,
+            crossOrigin: 'Anonymous',
+            init: function (params) {
+                ImageCaching.log('Initialization of ImageCaching');
+                for (var param in params) {
+                    ImageCaching[param] = params[param];
+                }
 
-  //Push notification button
-  var notificationBtnElement = document.getElementById('turn-on-notification');
-  var pushBtnElement = document.querySelector('.send-push');
-
-  //API key & GCM Token
-  var apiKey = 'AIzaSyANQgbxQIyOASMdVU24T-wthqHRiAj8lmE'; //replace with your own key
-  var gcmURL = 'https://android.googleapis.com/gcm/send';
-
-  //To check `push notification` is supported
-  function isPushSupported() {
-    //To check `push notification` permission is denied by user
-    if (Notification.permission === 'denied') {
-      console.warn('User has blocked push notification.');
-      return;
-    }
-
-    //Check `push notification` is supported or not
-    if (!('PushManager' in window)) {
-      console.error('Push notification isn\'t supported in your browser.');
-      return;
-    }
-
-    //Get `push notification` subscription
-    //If `serviceWorker` is registered and ready
-    navigator.serviceWorker.ready
-      .then(function (registration) {
-        registration.pushManager.getSubscription()
-        .then(function (subscription) {
-          //If already access granted, enable push button status
-          if (subscription) {
-            changePushStatus(true);
-          }
-          else {
-            changePushStatus(false);
-          }
-        })
-        .catch(function (error) {
-          console.error('Error occurred while enabling push ', error);
-        });
-      });
-  }
-
-  //To subscribe `push notification`
-  function subscribePush() {
-    navigator.serviceWorker.ready
-    .then(function(registration) {
-      if (!registration.pushManager) {
-        alert('Your browser doesn\'t support push notification.');
-        return;
-      }
-
-      //To subscribe `push notification` from push manager
-      registration.pushManager.subscribe({
-        userVisibleOnly: true //Always show notification when received
-      })
-      .then(function (subscription) {
-        console.info('Push notification subscribed.');
-        changePushStatus(true);
-      })
-      .catch(function (error) {
-        changePushStatus(false);
-        console.error('Push notification subscription error: ', error);
-      })
-    })
-  }
-
-  //To unsubscribe `push notification`
-  function unsubscribePush() {
-    navigator.serviceWorker.ready
-    .then(function(registration) {
-      //Get `push subscription`
-      registration.pushManager.getSubscription()
-      .then(function (subscription) {
-        //If no `push subscription`, then return
-        if(!subscription) {
-          console.error('Unable to unregister push notification.');
-          return;
-        }
-
-        //Unsubscribe `push notification`
-        subscription.unsubscribe()
-          .then(function () {
-            console.info('Push notification unsubscribed.');
-            changePushStatus(false);
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      })
-      .catch(function (error) {
-        console.error('Failed to unsubscribe push notification.');
-      });
-    })
-  }
-
-  //To change status
-  function changePushStatus(status) {
-    notificationBtnElement.dataset.checked = status;
-    notificationBtnElement.checked = status;
-    if (status) {
-      pushBtnElement.removeAttribute("disabled");
-    }
-    else {
-      pushBtnElement.setAttribute("disabled", true);
-    }
-  }
-
-  //Click event for subscribe notificationBtnElement
-  notificationBtnElement.addEventListener('click', function () {
-    var isBtnChecked = (notificationBtnElement.dataset.checked === 'true');
-    if (isBtnChecked) {
-      unsubscribePush();
-    }
-    else {
-      subscribePush();
-    }
-  });
-
-  //To generate curl command to send push notification
-  function curlCommand(subscription) {
-    var temp = subscription.endpoint.split('/');
-    var endpoint = temp[temp.length - 1];
-    var curlCommand = 'curl --header "Authorization: key=' + apiKey + '" --header Content-Type:"application/json" ' + gcmURL + ' -d "{\\"registration_ids\\":[\\"' + endpoint + '\\"]}"';
-    console.log("%c curl command to send push notification ", "background: #000; color: #fff; font-size: 13px;");
-    console.log(curlCommand);
-  }
-
-  //Form data with info to send to server
-  function sendPushNotification(subscription) {
-    navigator.serviceWorker.ready
-      .then(function(registration) {
-        //Get `push subscription`
-        registration.pushManager.getSubscription()
-        .then(function (subscription) {
-          curlCommand(subscription); //To log curl command
-
-          //Send `push notification` - source for below url `server.js`
-          fetch('https://progressive-web-application.herokuapp.com/send_notification', {
-            method: 'post',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
+                $(ImageCaching.selector).each(function(){
+                    ImageCaching.applyToImage($(this));
+                });
             },
-            body: JSON.stringify(subscription)
-          })
-          .then(function(response) {
-            return response.json();
-          })
-        })
-      })
-  }
+            getCacheKey:function(element){
+                if(element.attr(ImageCaching.cachingKeyAttribute)){
+                    return  element.attr(ImageCaching.cachingKeyAttribute);
+                }else{
+                    return element.attr(ImageCaching.sourceKeyAttribute);
+                }
+            },
+            getCache:function(element){
+                var key = this.getCacheKey(element);
+                return localStorage.getItem(key);
+            },
+            setCache:function(element,imageData){
+                var key = ImageCaching.getCacheKey(element);
+                ImageCaching.log('Set cache',key,imageData,element);
+                localStorage.setItem(key, imageData); // save image data
+                return true;
+            },
+            removeCache:function(element){
+                var key = ImageCaching.getCacheKey(element);
+                ImageCaching.log('Remove cache',key);
+                localStorage.removeItem(key);
+                return true;
+            },
+            renderImage:function(element,picture){
+                ImageCaching.log('Rendering...',picture,element);
+                element.attr('src',picture);
 
-  //To send `push notification`
-  pushBtnElement.addEventListener("click", function () {
-    sendPushNotification();
-  }, false);
+                if(this.renderCallback){
+                    ImageCaching.log('Render Callback...',element);
+                    this.renderCallback(picture,element);
+                }
+            },
+            applyToImage:function(element,force){
+                var cache = null;
+                if(!force){
+                    cache = this.getCache(element);
+                }
 
-  isPushSupported(); //Check for push notification support
-})(window);
+                if(cache){
+                    ImageCaching.log('Image from cache',element);
+                    this.renderImage(element,cache);
+                }else{
+                    var sourceLink = element.attr(ImageCaching.sourceKeyAttribute);
+                    var getParamPrefix = "?";
+                    if(sourceLink.indexOf('?')>0){
+                        getParamPrefix = "&";
+                    }
+                    sourceLink+=getParamPrefix+'cacheTime='+Date.now();
+
+                    ImageCaching.log('Request to: '+sourceLink,element);
+
+                    var img = new Image();
+
+                    if(ImageCaching.crossOrigin){
+                        img.setAttribute('crossOrigin', 'Anonymous');
+                    }
+
+                    img.onload  = function() {
+                        ImageCaching.log('Loading completed',img);
+                        var canvas = document.createElement('canvas');
+                        canvas.width = img.width;
+                        canvas.height = img.height;
+
+                        // Copy the image contents to the canvas
+                        var ctx = canvas.getContext("2d");
+                        ctx.drawImage(img, 0, 0);
+
+                        document.body.appendChild(canvas);
+                        var context = canvas.getContext('2d');
+                        context.drawImage(img, 0, 0);
+
+                        var imageData = canvas.toDataURL("image/png");
+                        ImageCaching.setCache(element,imageData);
+                        ImageCaching.renderImage(element, imageData);
+                    }
+                    img.src = sourceLink;
+                }
+            },
+            refresh:function(itemsSelector){
+                var selector = null;
+                if(itemsSelector){
+                    selector = itemsSelector;
+                }else{
+                    selector = ImageCaching.selector;
+                }
+
+                $(selector).each(function(){
+                    ImageCaching.applyToImage($(this),true);
+                });
+
+            },
+            log: function () {
+                if (this.debugMode) {
+                    console.log(arguments);
+                }
+            }
+
+        };
+        ImageCaching.init(params);
+        return ImageCaching;
+    }
+
+    $.fn.extend({
+        imageCaching: function(options) {
+            var params = {selector:this};
+            params = $.extend(params,options);
+
+            return new jQueryImageCaching(params);
+        }
+    });
+})(jQuery);
